34c2c8a718f4458d724e1bad5edff9deb5289c4e
Your Name <you@example.com>
2018-04-09 17:29:39 +0200

    Merge origin/master

--- OOPL6/src/main/java/pl/edu/ur/oopl6/zad1/SkladoweStatyczne.java
+++ OOPL6/src/main/java/pl/edu/ur/oopl6/zad1/SkladoweStatyczne.java
@@ -7,6 +7,18 @@ public class SkladoweStatyczne {
         
         System.out.println(Figury.PoleKola(0.5));
         System.out.println(Figury.ObwodKola(0.5));
+        System.out.println(Figury.PoleKwadratu(0.5));
+        System.out.println(Figury.ObwodKwadratu(0.5));
+        System.out.println(Figury.PoleProstokąta(0.5, 0.75));
+        System.out.println(Figury.ObwodProstokąta(0.5, 0.75));
+        System.out.println(Figury.PolePodstawyStożka(0.5));
+        System.out.println(Figury.PolePowierzchniBocznej(0.5, 0.75));
+        System.out.println(Figury.PolePowierzchniCałkowitej(0.5, 0.75));
+        System.out.println(Figury.ObjętośćStożka(0.5, 0.75));
+        System.out.println(Figury.PolePodstawyWalca(0.5));
+        System.out.println(Figury.PolePowierzchniBocznejWalca(0.5, 0.75));
+        System.out.println(Figury.PolePowierzchniCałkowitejWalca(0.5, 0.75));
+        System.out.println(Figury.ObjętośćWalca(0.5, 0.75));
     }
     
 }
